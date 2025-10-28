de737e62f289745b7c5c9a94476127daca4c47be
YeDemirkiran <yedemirkiran27@gmail.com>
2025-10-28 19:42:53 +0300

    Silah Ateş zamanlayıcısı eklendi

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -2,21 +2,32 @@ using UnityEngine;
 
 public class GunController : MonoBehaviour
 {
+    public float fireRate = 1f;
     public float bulletSpeed = 1000f;
     public Transform crosshair;
     public GameObject bulletPrefab;
 
-    // Update is called once per frame
+    private float timer = 0f;
+
     void Update()
     {
-        GameObject bullet = Instantiate(bulletPrefab);
+        if (timer > 1f / fireRate)
+        {
+            GameObject bullet = Instantiate(bulletPrefab);
 
-        bullet.transform.position = crosshair.transform.position;
-        bullet.transform.rotation = crosshair.transform.rotation;
+            bullet.transform.position = crosshair.transform.position;
+            bullet.transform.rotation = crosshair.transform.rotation;
 
-        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+            Rigidbody rb = bullet.GetComponent<Rigidbody>();
 
-        rb.AddForce(new Vector3(0f, 0f, bulletSpeed));
-        Destroy(bullet, 1.5f);
+            rb.AddForce(new Vector3(0f, 0f, bulletSpeed));
+            Destroy(bullet, 1.5f);
+
+            timer = 0f;
+        }
+        else
+        {
+            timer += Time.deltaTime;
+        }
     }
 }
