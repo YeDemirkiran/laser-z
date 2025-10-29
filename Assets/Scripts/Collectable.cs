59c3a8c41fd12ba0578591c640af9bcb09b70455
YeDemirkiran <yedemirkiran27@gmail.com>
2025-10-29 20:35:30 +0300

    UpgradeCollectable şeklini değiştir

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -3,43 +3,47 @@ using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
+    public enum Type { Health, Upgrade, Gun }
+
+    public Type type;
+    public TextMeshProUGUI text;
     public float healthGive = 25f;
     public int upgradeUnlock = 25;
-    private bool flag = false;
-    public TextMeshProUGUI text;
+
+    bool flag = false;
     bool disabled = false;
-    public enum TYPE { Healt, Upgrade, Gun}
-    public TYPE type;
+
 
     private void Start()
     {
-        if (type == TYPE.Healt)
+        if (type == Type.Health)
             text.text = $"{healthGive.ToString()}";
-        else if (type == TYPE.Upgrade)
+        else if (type == Type.Upgrade)
             text.text = $"{upgradeUnlock.ToString()}";
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        
-        Debug.LogWarning("Collectable hit: " + other.name);
-        if (!disabled && other.gameObject.CompareTag("Player"))
+        if (disabled)
+            return;
+
+        if (other.gameObject.CompareTag("Player"))
         {
             PlayerController player = other.attachedRigidbody.GetComponent<PlayerController>();
-            if (type == TYPE.Healt)
+            if (type == Type.Health)
                 player.health += healthGive;
-            else if (type == TYPE.Upgrade && flag == true)
+            else if (type == Type.Upgrade && flag == true)
                 player.GetComponentInChildren<GunController>().fireRate += 1;
             disabled = true;
         }
-        else if (!disabled && other.gameObject.CompareTag("Bullet"))
+        else if (other.gameObject.CompareTag("Bullet"))
         {
-            if (type == TYPE.Healt)
+            if (type == Type.Health)
             {
                 healthGive += 1f;
                 text.text = $"{healthGive.ToString()}";
             }
-            else if (type == TYPE.Upgrade && upgradeUnlock > 0)
+            else if (type == Type.Upgrade && upgradeUnlock > 0)
             {
                 upgradeUnlock -= 1;
                 text.text = $"{upgradeUnlock.ToString()}";
