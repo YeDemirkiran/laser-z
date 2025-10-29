d2c4d91626e2dcc6de40ff6808d071a8ee46b627
YeDemirkiran <yedemirkiran27@gmail.com>
2025-10-29 17:02:27 +0300

    Enemy hareket etme kodu eklendi

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -3,6 +3,18 @@ using UnityEngine;
 public class EnemyAI : MonoBehaviour
 {
     public float health = 100f;
+    public float speed = 5f;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            float sign = Mathf.Sign(other.transform.position.x - transform.position.x);
+            Vector3 pos = transform.position;
+            pos.x += speed * sign * Time.deltaTime;
+            transform.position = pos;
+        }
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
