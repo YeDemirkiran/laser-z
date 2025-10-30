8bcdbb3443b1451fe1956d5bdfe8a4419494c274
YeDemirkiran <yedemirkiran27@gmail.com>
2025-10-31 01:52:01 +0300

    Durdurma ve ölme ekran UI'ları eklendi

--- Assets/Scripts/EnemyHealthBar.cs
+++ Assets/Scripts/EnemyHealthBar.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class EnemyHealtBar : MonoBehaviour
+public class EnemyHealthBar : MonoBehaviour
 {
     public EnemyAI enemyAI;
     private Slider slider;
