51249041caf330d8aabe76c4cb4f2ac658b99ba3
YeDemirkiran <yedemirkiran27@gmail.com>
2025-10-30 20:13:31 +0300

    Yeni input sistemine geçiş yapıldı

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,27 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public float steerSpeed = 1f;
     public float health = 100f;
+    public InputActionReference moveAction;
+
+    float input = 0f;
+
+    private void OnEnable()
+    {
+        moveAction.action.Enable();
+        moveAction.action.performed += OnMove;
+        moveAction.action.canceled += OnMove;
+    }
+
+    private void OnDisable()
+    {
+        moveAction.action.Disable();
+        moveAction.action.performed -= OnMove;
+        moveAction.action.canceled -= OnMove;
+    }
 
     void Update()
     {
@@ -11,8 +29,18 @@ public class PlayerController : MonoBehaviour
             Destroy(gameObject);
         else if (health > 100f)
             health = 100f;
+
         Vector3 pos = transform.position;
-        pos.x += Input.GetAxisRaw("Horizontal") * steerSpeed * Time.deltaTime;
+        if (input > 1f)
+            input = 1f;
+        else if (input < -1f)
+            input = -1f;
+        pos.x += input * steerSpeed * Time.deltaTime;
         transform.position = pos;
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        input = context.ReadValue<float>();
     }
 }
